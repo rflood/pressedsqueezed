6b309d620c7b55515e1a97ff2fbfa337f29adb3f
rflood <rflood@mcad.edu>
2014-02-07 19:36:28 -0800

    Added <meta>, wp_head(), <body> and moved <script>
    Added     <meta http-equiv="content-type" content= "<?php
    bloginfo('html_type' ); ?>; charset=<?php bloginfo("charset"); ?>">
    
    <?php wp_head(); ?>
    
    <body <?php body_class ( $class ); ?>>
    
    And Moved
    
    <script src="http://use.edgefonts.net/josefin-slab.js"></script>
    <script src="http://use.edgefonts.net/merriweather.js"></script>
    
    beneath <title>

--- header.php
+++ header.php
@@ -3,13 +3,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Pressed and Squeezed</title>
+    <meta http-equiv="content-type" content= "<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo("charset"); ?>">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" >
+    <?php wp_head(); ?>
+    <title><?php bloginfo("name"); ?></title>
     <script src="http://use.edgefonts.net/josefin-slab.js"></script>
     <script src="http://use.edgefonts.net/merriweather.js"></script>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" >
-    <title><?php bloginfo("name"); ?></title>
 </head>
-<body>
+<body <?php body_class ( $class ); ?>>
 <header>
     <h1><?php bloginfo("name"); ?></h1>
     <nav>
